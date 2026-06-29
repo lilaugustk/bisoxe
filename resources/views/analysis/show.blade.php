e25aa804296f222e0a6c7c6af426ff9136fa671e
lilaugustk <lilaugustk@gmail.com>
2026-06-29 18:05:41 +0700

    ...

--- resources/views/analysis/show.blade.php
+++ resources/views/analysis/show.blade.php
@@ -552,15 +552,6 @@
                     </div>
 
                     <!-- Footer / Navigation -->
-                    <footer class="flex items-center justify-between text-xs">
-                        <a href="/top" class="inline-flex items-center gap-1.5 font-bold text-[#8C1E1E] hover:underline">
-                            <svg class="h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2.5">
-                                <path stroke-linecap="round" stroke-linejoin="round" d="M15 19l-7-7 7-7" />
-                            </svg>
-                            Quay lại danh sách Top
-                        </a>
-                    </footer>
-
                 </article>
                 
             </div>
